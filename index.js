da64d3696ec95bb4d61d77ce203c5c751862abc9
peaceboii <89594653+peaceboii@users.noreply.github.com>
2025-08-06 19:02:48 +0530

    added one more project

--- index.js
+++ index.js
@@ -2,17 +2,8 @@
   $(".hamburger").click(function () {
     $("#navMenu").toggleClass("show-nav");
   });
-
-
-  $('.fill').each(function () {
-    var elementTop = $(this).offset().top;
-    var viewportBottom = $(window).scrollTop() + $(window).height();
-
-    if (elementTop < viewportBottom - 50 && !$(this).hasClass("animated")) {
-      var percent = $(this).attr("data-percent");
-      $(this).css("width", percent);
-      $(this).addClass("animated");
+  $(document).click(function () {
+    if (!$(event.target).closest(".hamburger, #navMenu").length) {
+      $("#navMenu").removeClass("show-nav");
     }
   });
-
-  
